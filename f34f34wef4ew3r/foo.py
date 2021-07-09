f1317ff4da5f5c70ddd907cc76645f3a89c30189
Moneypulation <haxor@DESKTOP-FCPF7PJ.localdomain>
2021-07-09 02:44:28 +0200

    bla6

--- f34f34wef4ew3r/foo.py
+++ f34f34wef4ew3r/foo.py
@@ -1,2 +1,2 @@
 def foo123():
-    print("ok new 1.5")
+    print("ok new 1.7")
--- setup.py
+++ setup.py
@@ -8,46 +8,20 @@ def _post_install():
   print("owned-post")
   with open("/tmp/owned-ins","w") as f:
     f.write("owned-post")
-
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
-    def run(self):
-        develop.run(self)
-        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-        print("owned-dev")
-        with open("owned-dev","w") as f:
-          f.write("owned-dev")
-          
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        install.run(self)
-        # PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-        print("owned-ins")
-        with open("owned-ins","w") as f:
-          f.write("owned-ins")
-          
-
-
     
 setup(
   name = 'f34f34wef4ew3r',         # How you named your package folder (MyLib)
   packages = ['f34f34wef4ew3r'],   # Chose the same as "name"
-  version = '1.5',      # Start with a small number and increase it with every change you make
+  version = '1.7',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'YOUR NAME',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Moneypulation/f34f34wef4ew3r/archive/refs/tags/v_14.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Moneypulation/f34f34wef4ew3r/archive/refs/tags/v_15.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
-      cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
-    },
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
